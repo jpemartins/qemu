5d6b2e19fdb63c85e1fd2b5732d5a684ec871111
Ankur Arora <ankur.a.arora@oracle.com>
2018-08-17 16:52:05 -0700

    kvm/ioapic: mark gsi-2 used in ioapic routing init
    GSI-2/IOAPIC pin-2 is treated specially while initing
    IRQ routing: PIC does not use it at all while the IOAPIC
    maps virq=0 to pin-2 and does not use GSI-2.
    (all other GSIs are identity mapped to pins.)
    
    This results in any later code which allocates a virq
    to be assigned GSI-2. This virq is in-turn used to
    remap interrupts to HYPERVISOR_CALLBACK_VECTOR (0xf3)
    to deliver to the guest.
    
    Ordinarily this would be okay, but if the event delivery is
    via direct injection via KVM_REQ_EVENT (without going
    through the LAPIC) we see vmentry failure.
    
    This works fine for any other values of GSI.
    
    As a workaround, mark GSI-2 used.
    
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>

--- include/sysemu/kvm.h
+++ include/sysemu/kvm.h
@@ -496,6 +496,7 @@ int kvm_irqchip_update_msi_route(KVMState *s, int virq, MSIMessage msg,
                                  PCIDevice *dev);
 void kvm_irqchip_commit_routes(KVMState *s);
 void kvm_irqchip_release_virq(KVMState *s, int virq);
+void kvm_irqchip_set_gsi(KVMState *s, unsigned int gsi);
 
 int kvm_irqchip_add_adapter_route(KVMState *s, AdapterInfo *adapter);
 int kvm_irqchip_add_hv_sint_route(KVMState *s, uint32_t vcpu, uint32_t sint);

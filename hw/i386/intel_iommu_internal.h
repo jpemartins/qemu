3e9d2274ffe3a4020b647bb992fcce688ced96d3
Joao Martins <joao.m.martins@oracle.com>
2022-02-04 07:51:59 -0500

    intel_iommu: Second Stage Access Dirty bit support
    IOMMU advertises Access/Dirty bits if the extended capability
    DMAR register reports it (ECAP, mnemonic ECAP.SSADS albeit it used
    to be known as SLADS before). The first stage table, though, has no bit for
    advertising Access/Dirty, unless referenced via a scalable-mode PASID Entry.
    Relevant Intel IOMMU SDM ref for first stage table "3.6.2 Accessed, Extended
    Accessed, and Dirty Flags" and second stage table "3.7.2 Accessed and Dirty
    Flags".
    
    To enable it we depend on scalable-mode for the second-stage table,
    so we limit use of dirty-bit to scalable-mode To use SSADS, we set a bit in
    the scalable-mode PASID Table entry, by setting bit 9 (SSADE). When
    we do so we require flushing the context/pasid-table caches and IOTLB much
    like AMD. Relevant SDM refs:
    
    "3.7.2 Accessed and Dirty Flags"
    "6.5.3.3 Guidance to Software for Invalidations,
     Table 23. Guidance to Software for Invalidations"
    
    To read out what's dirtied, same thing as past implementations is done.
    Dirty bit support is located in the same location (bit 9). The IOTLB
    caches some attributes when SSADE is enabled and dirty-ness information,
    so we also need to flush IOTLB to make sure IOMMU attempts to set the
    dirty bit again. Relevant manuals over the hardware translation is
    chapter 6 with some special mention to:
    
    "6.2.3.1 Scalable-Mode PASID-Table Entry Programming Considerations"
    "6.2.4 IOTLB"
    
    The first 12bits of the PTE are already cached via the SLPTE pointer,
    similar to how it is added in amd-iommu. Use also the previously
    added PASID entry cache in order to fetch whether Dirty bit was
    enabled or not in the SM second stage table.
    
    This is useful for covering and prototyping IOMMU support for access/dirty
    bits.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- hw/i386/intel_iommu_internal.h
+++ hw/i386/intel_iommu_internal.h
@@ -192,6 +192,7 @@
 #define VTD_ECAP_MHMV               (15ULL << 20)
 #define VTD_ECAP_SRS                (1ULL << 31)
 #define VTD_ECAP_SMTS               (1ULL << 43)
+#define VTD_ECAP_SLADS              (1ULL << 45)
 #define VTD_ECAP_SLTS               (1ULL << 46)
 
 /* CAP_REG */
@@ -492,6 +493,7 @@ typedef struct VTDRootEntry VTDRootEntry;
 
 #define VTD_SM_PASID_ENTRY_AW          7ULL /* Adjusted guest-address-width */
 #define VTD_SM_PASID_ENTRY_DID(val)    ((val) & VTD_DOMAIN_ID_MASK)
+#define VTD_SM_PASID_ENTRY_SLADE       (1ULL << 9)
 
 /* Second Level Page Translation Pointer*/
 #define VTD_SM_PASID_ENTRY_SLPTPTR     (~0xfffULL)
@@ -515,5 +517,7 @@ typedef struct VTDRootEntry VTDRootEntry;
 #define VTD_SL_PT_BASE_ADDR_MASK(aw) (~(VTD_PAGE_SIZE - 1) & VTD_HAW_MASK(aw))
 #define VTD_SL_IGN_COM              0xbff0000000000000ULL
 #define VTD_SL_TM                   (1ULL << 62)
+#define VTD_SL_A                    (1ULL << 8)
+#define VTD_SL_D                    (1ULL << 9)
 
 #endif

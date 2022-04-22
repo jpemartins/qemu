8c875e9e4ea54aad72430f4c133615629f93f9eb
Joao Martins <joao.m.martins@oracle.com>
2022-04-22 11:10:41 -0400

    vfio/iommufd: Add HWPT_SET_DIRTY support
    ioctl(iommufd, IOMMU_HWPT_SET_DIRTY, arg) is the UAPI
    that enables or disables dirty page tracking. We set it
    on the whole list of iommu domains we are tracking, and
    set ::dirty_pages_supported accordingly, used when we
    attempt at reading out the dirty bits from the hw pagetables.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- hw/vfio/iommufd.c
+++ hw/vfio/iommufd.c
@@ -35,6 +35,7 @@
 #include "sysemu/reset.h"
 #include "qemu/cutils.h"
 #include "qemu/char_dev.h"
+#include "migration/migration.h"
 
 static bool iommufd_check_extension(VFIOContainer *bcontainer,
                                     VFIOContainerFeature feat)
@@ -111,6 +112,26 @@ static int iommufd_unmap(VFIOContainer *bcontainer,
     /* TODO: Handle dma_unmap_bitmap with iotlb args (migration) */
     return iommufd_backend_unmap_dma(container->be,
                                      container->ioas_id, iova, size);
+}
+
+static int iommufd_set_dirty_page_tracking(VFIOContainer *bcontainer,
+                                           bool start)
+{
+    VFIOIOMMUFDContainer *container = container_of(bcontainer,
+                                                   VFIOIOMMUFDContainer, bcontainer);
+    int ret;
+    VFIOIOASHwpt *hwpt;
+
+    QLIST_FOREACH(hwpt, &container->hwpt_list, next) {
+        ret = iommufd_backend_set_dirty_tracking(container->be,
+                                                 hwpt->hwpt_id, start);
+        if (ret) {
+            return ret;
+        }
+    }
+
+    bcontainer->dirty_pages_supported = start;
+    return 0;
 }
 
 static int vfio_get_devicefd(const char *sysfs_path, Error **errp)
@@ -507,6 +528,7 @@ static int iommufd_attach_device(VFIODevice *vbasedev, AddressSpace *as,
             "memory listener initialization failed: ");
         goto error;
     }
+    bcontainer->dirty_pages_supported = true;
     bcontainer->initialized = true;
 
 out:
@@ -621,6 +643,7 @@ static void vfio_iommu_backend_iommufd_ops_class_init(ObjectClass *oc,
     ops->dma_unmap = iommufd_unmap;
     ops->attach_device = iommufd_attach_device;
     ops->detach_device = iommufd_detach_device;
+    ops->set_dirty_page_tracking = iommufd_set_dirty_page_tracking;
 }
 
 static int vfio_iommu_device_attach_hwpt(IOMMUFDDevice *idev,
--- include/sysemu/iommufd.h
+++ include/sysemu/iommufd.h
@@ -51,4 +51,7 @@ int iommufd_backend_alloc_hwpt(int iommufd, uint32_t dev_id,
                                uint32_t *out_hwpt);
 int iommufd_backend_invalidate_cache(int iommufd, uint32_t hwpt_id,
                                      uint32_t len, void *data_ptr);
+int iommufd_backend_set_dirty_tracking(IOMMUFDBackend *be, uint32_t hwpt_id,
+                                       bool start);
+
 #endif

8a7fb1106a6759095a638c4fc3e369768ad3d3e1
Joao Martins <joao.m.martins@oracle.com>
2022-04-22 11:10:41 -0400

    vfio/iommufd: Implement VFIOIOMMUClass::set_dirty_tracking support
    ioctl(iommufd, IOMMU_HWPT_SET_DIRTY_TRACKING, arg) is the UAPI that
    enables or disables dirty page tracking.
    
    It is called on the whole list of iommu domains it is are tracking,
    and on failure it rolls it back.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- backends/iommufd.c
+++ backends/iommufd.c
@@ -240,6 +240,25 @@ int iommufd_backend_alloc_hwpt(IOMMUFDBackend *be, uint32_t dev_id,
     return !ret ? 0 : -errno;
 }
 
+int iommufd_backend_set_dirty_tracking(IOMMUFDBackend *be, uint32_t hwpt_id,
+                                       bool start)
+{
+    int ret;
+    struct iommu_hwpt_set_dirty_tracking set_dirty = {
+            .size = sizeof(set_dirty),
+            .hwpt_id = hwpt_id,
+            .flags = !start ? 0 : IOMMU_HWPT_DIRTY_TRACKING_ENABLE,
+    };
+
+    ret = ioctl(be->fd, IOMMU_HWPT_SET_DIRTY_TRACKING, &set_dirty);
+    trace_iommufd_backend_set_dirty(be->fd, hwpt_id, start, ret);
+    if (ret) {
+        error_report("IOMMU_HWPT_SET_DIRTY_TRACKING failed: %s",
+                     strerror(errno));
+    }
+    return !ret ? 0 : -errno;
+}
+
 static const TypeInfo iommufd_backend_info = {
     .name = TYPE_IOMMUFD_BACKEND,
     .parent = TYPE_OBJECT,

fbc8f2e02d0e38d37e90d23f164f811c970a8db5
Avihai Horon <avihaih@nvidia.com>
2022-05-23 11:11:21 -0400

    vfio/migration: Implement VFIO migration protocol v2
    Replace the current VFIO migration protocol v1 implementation with a new
    implementation corresponding to VFIO migration protocol v2.
    
    The main changes are:
    - VFIO device state is now represented as a finite state machine instead
      of a bitmap.
    
    - Migration interface with kernel is now done using VFIO_DEVICE_FEATURE
      ioctl and normal read() and write() instead of the migration region.
    
    - As VFIO migration protocol v2 currently doesn't support the pre-copy
      phase of migration, .save_live_pending and .save_live_iterate handlers
      plus pre-copy relevant code are removed.
    
    Detailed information about VFIO migration protocol v2 and difference
    compared to v1 can be found here [1].
    
    [1]
    https://lore.kernel.org/all/20220224142024.147653-10-yishaih@nvidia.com/
    
    Signed-off-by: Avihai Horon <avihaih@nvidia.com>
    [joaomart: Add prefixes to v2 protocol functions, add ::v2 field]
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -61,11 +61,17 @@ typedef struct VFIORegion {
 typedef struct VFIOMigration {
     struct VFIODevice *vbasedev;
     VMChangeStateEntry *vm_state;
+    /* v1 */
     VFIORegion region;
     uint32_t device_state;
     int vm_running;
     Notifier migration_state;
     uint64_t pending_bytes;
+    /* v2 */
+    bool v2;
+    int data_fd;
+    void *data_buffer;
+    size_t data_buffer_size;
 } VFIOMigration;
 
 typedef struct VFIOAddressSpace {

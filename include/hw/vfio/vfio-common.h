c66caa0ad825c2366c27a8ca498351daa013fa57
Avihai Horon <avihaih@nvidia.com>
2022-05-23 11:29:16 -0400

    vfio/migration: Remove VFIO migration protocol v1
    Signed-off-by: Avihai Horon <avihaih@nvidia.com>
    [joaomart: mainly extracted from predecessor patch]
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -61,14 +61,8 @@ typedef struct VFIORegion {
 typedef struct VFIOMigration {
     struct VFIODevice *vbasedev;
     VMChangeStateEntry *vm_state;
-    /* v1 */
-    VFIORegion region;
-    uint32_t device_state;
-    int vm_running;
     Notifier migration_state;
-    uint64_t pending_bytes;
-    /* v2 */
-    bool v2;
+    enum vfio_device_mig_state device_state;
     int data_fd;
     void *data_buffer;
     size_t data_buffer_size;

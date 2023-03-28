e364fb2237ae46861fbdd8b27cc8ecf737346c8d
Avihai Horon <avihaih@nvidia.com>
2023-03-28 16:03:30 +0300

    vfio/migration: Move from STOP_COPY to STOP in vfio_save_cleanup()
    Changing device state from STOP_COPY to STOP can take time as the device
    might need to free resources etc. Currently, this is done in
    vfio_save_complete_precopy() and therefore is counted in the migration
    downtime.
    
    To avoid this and reduce migration downtime, change the device state
    from STOP_COPY to STOP in vfio_save_cleanup() which is not part of
    migration downtime.
    
    Signed-off-by: Avihai Horon <avihaih@nvidia.com>
    Change-Id: I37b6a30dfed11c79e65a837fcf4b42e6432eaa84
    (cherry picked from commit 1b75e0f702510a8d54133c09641b1d81df463264)
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- hw/vfio/migration.c
+++ hw/vfio/migration.c
@@ -376,6 +376,20 @@ static void vfio_save_cleanup(void *opaque)
     VFIODevice *vbasedev = opaque;
     VFIOMigration *migration = vbasedev->migration;
 
+    /*
+     * Changing device state from STOP_COPY to STOP can take time (device might
+     * need to free resources etc.). Instead of doing it in
+     * vfio_save_complete_precopy(), do it here outside of downtime.
+     */
+    if (migration->device_state == VFIO_DEVICE_STATE_STOP_COPY) {
+        /*
+         * If setting the device in STOP state fails, the device should be
+         * reset. To do so, use ERROR state as a recover state.
+         */
+        vfio_migration_set_state(vbasedev, VFIO_DEVICE_STATE_STOP,
+                                 VFIO_DEVICE_STATE_ERROR);
+    }
+
     g_free(migration->data_buffer);
     migration->data_buffer = NULL;
     vfio_migration_cleanup(vbasedev);
@@ -501,12 +515,6 @@ static int vfio_save_complete_precopy(QEMUFile *f, void *opaque)
         return ret;
     }
 
-    /*
-     * If setting the device in STOP state fails, the device should be reset.
-     * To do so, use ERROR state as a recover state.
-     */
-    ret = vfio_migration_set_state(vbasedev, VFIO_DEVICE_STATE_STOP,
-                                   VFIO_DEVICE_STATE_ERROR);
     trace_vfio_save_complete_precopy(vbasedev->name, ret);
 
     return ret;

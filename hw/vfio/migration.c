d882ce570a96de5edefa2c897fb96473574f9188
Joao Martins <joao.m.martins@oracle.com>
2024-02-09 19:07:55 +0000

    vfio/common: Allow disabling device dirty page tracking
    The property 'x-pre-copy-dirty-page-tracking' allows disabling the whole
    tracking of VF pre-copy phase of dirty page tracking, though it means
    that it will only be used at the start of the switchover phase.
    
    Add an option that disables the VF dirty page tracking, and fall
    back into container-based dirty page tracking. This also allows to
    use IOMMU dirty tracking even on VFs with their own dirty
    tracker scheme.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- hw/vfio/migration.c
+++ hw/vfio/migration.c
@@ -938,7 +938,8 @@ bool vfio_migration_realize(VFIODevice *vbasedev, Error **errp)
         return !vfio_block_migration(vbasedev, err, errp);
     }
 
-    if (!vbasedev->dirty_pages_supported &&
+    if ((!vbasedev->dirty_pages_supported ||
+         vbasedev->device_dirty_page_tracking == ON_OFF_AUTO_OFF) &&
         (vbasedev->iommufd_dev.iommufd &&
          !iommufd_dirty_pages_supported(&vbasedev->iommufd_dev, &err))) {
         if (vbasedev->enable_migration == ON_OFF_AUTO_AUTO) {
